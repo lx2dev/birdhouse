f0aa0375a749626a9672ba7b0e65a8b4d77d862b
SlickYeet <lasse@famlam.ca>
2025-12-12 21:28:18 -0800

    feat: add validation schemas for sign-in and sign-up forms

---
+++ src/modules/auth/schemas/auth.ts
@@ -0,0 +1,12 @@
+import z from "zod"
+
+export const SignInSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(12, "Password must be at least 12 characters long"),
+})
+
+export const SignUpSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(12, "Password must be at least 12 characters long"),
+  username: z.string().min(1, "Username is required"),
+})
