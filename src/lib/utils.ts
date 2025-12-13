3b1cbec6b7a656a6fa0ead5d5e379684f17a59d4
SlickYeet <lasse@famlam.ca>
2025-12-12 17:53:57 -0800

    feat: re-add getBaseUrl function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,14 @@ import type { ClassValue } from "clsx"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { env } from "@/env"
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getBaseUrl() {
+  if (typeof window !== "undefined") return window.location.origin
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`
+  return env.NEXT_PUBLIC_URL
 }
