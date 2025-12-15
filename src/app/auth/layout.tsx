a51116efab958ef7f6108fa4c583844a67e61691
SlickYeet <lasse@famlam.ca>
2025-12-14 22:32:06 -0800

    feat: implement session check in AuthLayout to redirect authenticated users

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link"
+import { redirect } from "next/navigation"
 
 import { Icons } from "@/components/icons"
+import { getSession } from "@/lib/auth/utils"
 
-export default function AuthLayout({ children }: LayoutProps<"/auth">) {
+export default async function AuthLayout({ children }: LayoutProps<"/auth">) {
+  const session = await getSession()
+  if (session) return redirect("/dashboard")
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-2 md:gap-6">
