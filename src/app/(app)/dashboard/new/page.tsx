6d0db217630a04310c8de83824a12b58a1b8b797
SlickYeet <lasse@famlam.ca>
2025-12-14 17:40:13 -0800

    feat: add prefetch for sshKey list in NewComputePage

--- src/app/(app)/dashboard/new/page.tsx
+++ src/app/(app)/dashboard/new/page.tsx
@@ -3,6 +3,7 @@ import { NewComputeView } from "@/modules/dashboard/views/new-compute"
 
 export default function NewComputePage() {
   void api.template.list.prefetch()
+  void api.sshKey.list.prefetch()
 
   return (
     <HydrateClient>
