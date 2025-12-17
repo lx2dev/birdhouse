066b6f846519ec689ba571d1dde4c3c6984b25fe
SlickYeet <lasse@famlam.ca>
2025-12-16 19:20:02 -0800

    feat: relocate osRouter to dashboard module and update import paths

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,7 @@
 import { adminRouter } from "@/modules/admin/server/api/admin"
 import { computeRouter } from "@/modules/dashboard/server/api/compute"
+import { osRouter } from "@/modules/dashboard/server/api/os"
 import { createCallerFactory, createTRPCRouter } from "@/server/api/init"
-import { osRouter } from "@/server/api/routers/os"
 import { sshKeyRouter } from "@/server/api/routers/sshkey"
 import { templateRouter } from "@/server/api/routers/template"
 
