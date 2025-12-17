c7806b4128b6221207faaa62403b046d0497c839
SlickYeet <lasse@famlam.ca>
2025-12-16 19:21:21 -0800

    feat: relocate SSH key and template routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,9 +1,9 @@
 import { adminRouter } from "@/modules/admin/server/api/admin"
 import { computeRouter } from "@/modules/dashboard/server/api/compute"
 import { osRouter } from "@/modules/dashboard/server/api/os"
+import { sshKeyRouter } from "@/modules/dashboard/server/api/sshkey"
+import { templateRouter } from "@/modules/dashboard/server/api/template"
 import { createCallerFactory, createTRPCRouter } from "@/server/api/init"
-import { sshKeyRouter } from "@/server/api/routers/sshkey"
-import { templateRouter } from "@/server/api/routers/template"
 
 export const appRouter = createTRPCRouter({
   admin: adminRouter,
