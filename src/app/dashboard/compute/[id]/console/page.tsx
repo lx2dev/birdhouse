3f416a1435140fcb54b43b430c00dc91d32cf415
SlickYeet <lasse@famlam.ca>
2025-12-18 00:19:24 -0800

    feat: refactor InstanceConsolePage to use InstanceConsoleView and prefetch VNC access

--- src/app/dashboard/compute/[id]/console/page.tsx
+++ src/app/dashboard/compute/[id]/console/page.tsx
@@ -1,13 +1,21 @@
+import { notFound } from "next/navigation"
+
+import { api, HydrateClient } from "@/lib/api/server"
+import InstanceConsoleView from "@/modules/dashboard/views/instance-console"
+
 export default async function InstanceConsolePage({
   params,
 }: PageProps<"/dashboard/compute/[id]/console">) {
   const { id } = await params
 
-  return (
-    <div>
-      <h1>InstanceConsolePage</h1>
+  if (!id) return notFound()
 
-      <p>{id}</p>
-    </div>
+  void api.compute.getInstance.prefetch({ id })
+  void api.console.getVNCAccess.prefetch({ id })
+
+  return (
+    <HydrateClient>
+      <InstanceConsoleView id={id} />
+    </HydrateClient>
   )
 }
