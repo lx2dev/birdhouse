27212260254952c14d0987089c79bb6865c89dc1
SlickYeet <lasse@famlam.ca>
2025-12-18 00:18:19 -0800

    feat: add consoleRouter for VNC access management

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { adminRouter } from "@/modules/admin/server/api/admin"
 import { computeRouter } from "@/modules/dashboard/server/api/compute"
+import { consoleRouter } from "@/modules/dashboard/server/api/console"
 import { osRouter } from "@/modules/dashboard/server/api/os"
 import { sshKeyRouter } from "@/modules/dashboard/server/api/sshkey"
 import { templateRouter } from "@/modules/dashboard/server/api/template"
@@ -8,6 +9,7 @@ import { createCallerFactory, createTRPCRouter } from "@/server/api/init"
 export const appRouter = createTRPCRouter({
   admin: adminRouter,
   compute: computeRouter,
+  console: consoleRouter,
   os: osRouter,
   sshKey: sshKeyRouter,
   template: templateRouter,
