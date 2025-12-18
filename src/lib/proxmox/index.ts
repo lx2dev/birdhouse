0e321d1e90960bec5de389c3e7a6e65c3f5f22df
SlickYeet <lasse@famlam.ca>
2025-12-17 18:40:41 -0800

    fix: correct NODE_TLS_REJECT_UNAUTHORIZED assignment and add logging

--- src/lib/proxmox/index.ts
+++ src/lib/proxmox/index.ts
@@ -2,7 +2,10 @@ import { proxmoxApi } from "proxmox-api"
 
 import { env } from "@/env"
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = env.PM_TLS_SKIP_VERIFY ? "0" : "1"
+process.env.NODE_TLS_REJECT_UNAUTHORIZED =
+  env.PM_TLS_SKIP_VERIFY === "true" ? "0" : "1"
+
+console.log(process.env.NODE_TLS_REJECT_UNAUTHORIZED)
 
 export const proxmox = proxmoxApi({
   host: env.PM_HOST,
