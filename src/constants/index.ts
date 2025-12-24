25468a948bffb4eac1914711d8524d472fa2e689
SlickYeet <lasse@famlam.ca>
2025-12-23 22:37:30 -0800

    feat: define NAV_ITEMS for navigation menu

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,13 @@
+import { IconKey, IconPlus, IconServer2 } from "@tabler/icons-react"
+
 export const DEFAULT_FETCH_LIMIT = 10
 
 export const PM_DEFAULT_NODE = "pve01"
 export const PM_DEFAULT_POOL = "UserPool"
+
+export const NAV_ITEMS = [
+  { href: "/dashboard", icon: IconServer2, label: "Dashboard" },
+  { href: "/dashboard/new", icon: IconPlus, label: "Create Instance" },
+  { href: "/dashboard/ssh-keys", icon: IconKey, label: "SSH Keys" },
+]
+export type NavItem = (typeof NAV_ITEMS)[number]
