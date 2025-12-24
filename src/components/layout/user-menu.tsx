fedc5b869588e33150cac83d528f00e914507d53
SlickYeet <lasse@famlam.ca>
2025-12-23 17:26:08 -0800

    fix: set nativeButton prop to false in DropdownMenuTrigger for UserMenu component

--- src/components/layout/user-menu.tsx
+++ src/components/layout/user-menu.tsx
@@ -56,6 +56,7 @@ export function UserMenu({ user }: UserMenuProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
+        nativeButton={false}
         render={
           <div className="relative">
             <Avatar className="size-8">
