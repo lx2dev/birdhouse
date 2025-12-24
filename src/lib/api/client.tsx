9a038a93eea2d08c2014b96d9825e912f05b9778
SlickYeet <lasse@famlam.ca>
2025-12-23 18:19:46 -0800

    fix: correct import and usage of httpBatchLink and SuperJSON in TRPC client

--- src/lib/api/client.tsx
+++ src/lib/api/client.tsx
@@ -2,11 +2,11 @@
 
 import type { QueryClient } from "@tanstack/react-query"
 import { QueryClientProvider } from "@tanstack/react-query"
-import { httpBatchStreamLink, loggerLink } from "@trpc/client"
+import { httpBatchLink, loggerLink } from "@trpc/client"
 import { createTRPCReact } from "@trpc/react-query"
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
 import { useState } from "react"
-import SuperJSON from "superjson"
+import superjson from "superjson"
 
 import { getBaseUrl } from "@/lib/utils"
 import type { AppRouter } from "@/server/api/root"
@@ -40,13 +40,13 @@ export function TRPCReactProvider(props: { children: React.ReactNode }) {
             process.env.NODE_ENV === "development" ||
             (op.direction === "down" && op.result instanceof Error),
         }),
-        httpBatchStreamLink({
+        httpBatchLink({
           headers: () => {
             const headers = new Headers()
             headers.set("x-trpc-source", "nextjs-react")
             return headers
           },
-          transformer: SuperJSON,
+          transformer: superjson,
           url: `${getBaseUrl()}/api/trpc`,
         }),
       ],
