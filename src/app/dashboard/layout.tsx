b1c16a6e9f7116da327395e8ea3a4a199b3940dc
SlickYeet <lasse@famlam.ca>
2025-12-23 22:37:55 -0800

    feat: wrap AppHeader and AppSidebar in SidebarProvider for improved layout management

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,8 +2,10 @@ import type { UserWithRole } from "better-auth/plugins"
 import { redirect } from "next/navigation"
 
 import { SiteFooter } from "@/components/layout/site-footer"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import { getSession } from "@/lib/auth/utils"
 import { AppHeader } from "@/modules/dashboard/ui/app-header"
+import { AppSidebar } from "@/modules/dashboard/ui/app-sidebar"
 
 export default async function AppLayout({ children }: LayoutProps<"/">) {
   const session = await getSession()
@@ -11,8 +13,11 @@ export default async function AppLayout({ children }: LayoutProps<"/">) {
 
   return (
     <div className="min-h-svh">
-      <AppHeader user={session.user as UserWithRole} />
-      <main className="p-4 lg:p-8">{children}</main>
+      <SidebarProvider className="flex flex-col" defaultOpen={false}>
+        <AppHeader user={session.user as UserWithRole} />
+        <AppSidebar />
+        <main className="size-full p-4 lg:p-8">{children}</main>
+      </SidebarProvider>
       <SiteFooter />
     </div>
   )
