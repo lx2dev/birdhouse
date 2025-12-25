d3288cbe3d9bae890d0c3dc7d7ff40b19d0ceb58
SlickYeet <lasse@famlam.ca>
2025-12-25 01:57:59 -0800

    refactor: remove InstanceConsolePage and InstanceConsoleView components

--- src/modules/dashboard/views/instance-console.tsx
+++
@@ -1,9 +0,0 @@
-import { InstanceConsole } from "@/modules/dashboard/sections/instance/console"
-
-export default function InstanceConsoleView({ id }: { id: string }) {
-  return (
-    <div className="mx-auto max-w-7xl space-y-6">
-      <InstanceConsole id={id} />
-    </div>
-  )
-}
