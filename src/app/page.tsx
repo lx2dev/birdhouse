8b44b6e0771ca4de3abfea91b2a617d70fa02da9
SlickYeet <lasse@famlam.ca>
2026-02-11 15:08:59 -0800

    feat: update HomePage to render a dashboard link instead of redirecting to signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import { redirect } from "next/navigation"
+import { IconArrowRight } from "@tabler/icons-react"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
 
 export default function HomePage() {
-  return redirect("/auth/signin")
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <Button render={<Link href="/dashboard" />} variant="link">
+        Go to Dashboard <IconArrowRight />
+      </Button>
+    </div>
+  )
 }
