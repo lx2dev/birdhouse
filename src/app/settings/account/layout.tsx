22cb0bd9d9264500d1c03e7f0dd27d777f0c8305
SlickYeet <lasse@famlam.ca>
2026-02-21 20:54:55 -0800

    feat: implement AccountLayout component for user account settings

---
+++ src/app/settings/account/layout.tsx
@@ -0,0 +1,33 @@
+import type { UserWithRole } from "better-auth/plugins"
+import { redirect } from "next/navigation"
+
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { getSession } from "@/lib/auth/utils"
+import AccountHeader from "@/modules/settings/account/ui/account-header"
+import { AccountSidebar } from "@/modules/settings/account/ui/account-sidebar"
+
+export default async function AccountLayout({
+  children,
+}: LayoutProps<"/settings/account">) {
+  const session = await getSession()
+  if (!session) return redirect("/auth/signin")
+
+  return (
+    <div className="min-h-svh">
+      <SidebarProvider
+        style={
+          {
+            "--header-height": "calc(var(--spacing) * 12)",
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+          } as React.CSSProperties
+        }
+      >
+        <AccountSidebar />
+        <SidebarInset>
+          <AccountHeader user={session.user as UserWithRole} />
+          <main className="size-full p-4 lg:p-8">{children}</main>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  )
+}
