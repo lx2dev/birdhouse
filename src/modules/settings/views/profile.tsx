e3d6d68ba90825e020373fe3dc493f26c9913cc0
SlickYeet <lasse@famlam.ca>
2026-02-25 21:30:21 -0800

    refactor: update separators in ProfileView for consistent spacing and add PhoneNumberSection

--- src/modules/settings/views/profile.tsx
+++ src/modules/settings/views/profile.tsx
@@ -1,18 +1,20 @@
 import { Separator } from "@/components/ui/separator"
 import { EmailAddressesSection } from "@/modules/settings/sections/email-addresses"
+import { PhoneNumberSection } from "@/modules/settings/sections/phone-number"
 import { UserInfoSection } from "@/modules/settings/sections/user-info"
 
 export function ProfileView() {
   return (
     <div className="@container py-6">
       <UserInfoSection />
-      <Separator className="my-6" />
+      <Separator className="my-8" />
       <EmailAddressesSection />
-      <Separator className="my-6" />
+      <Separator className="my-8" />
+      <PhoneNumberSection />
+      <Separator className="my-8" />
 
       {/* 
         TODO: Add more sections here:
-        - Phone number
         - Connected accounts
       */}
     </div>
