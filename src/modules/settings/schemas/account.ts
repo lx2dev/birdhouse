f09f708900fef960f1c05ec5a0501e62aa3a0838
SlickYeet <lasse@famlam.ca>
2026-02-26 21:48:47 -0800

    refactor: import account schema for account insertion configuration

--- src/modules/settings/schemas/account.ts
+++ src/modules/settings/schemas/account.ts
@@ -1,5 +1,7 @@
 import { createInsertSchema } from "drizzle-zod"
 
-import { user } from "@/server/db/schema"
+import { account, user } from "@/server/db/schema"
 
 export const userInsertSchema = createInsertSchema(user)
+
+export const accountInsertSchema = createInsertSchema(account)
