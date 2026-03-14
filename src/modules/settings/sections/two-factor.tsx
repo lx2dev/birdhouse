17e0de1f64857f55f5aecc579c83f4a93e0782bd
SlickYeet <lasse@lx2.dev>
2026-03-13 23:29:52 -0700

    feat: update skeleton component for two-factor authentication section

--- src/modules/settings/sections/two-factor.tsx
+++ src/modules/settings/sections/two-factor.tsx
@@ -74,7 +74,6 @@ TwoFactorSection.Skeleton = () => (
       </h2>
     </div>
     <div className="space-y-2">
-      <Skeleton className="h-5 w-24 rounded-full" />
       <Skeleton className="h-4 w-64" />
     </div>
     <div className="@md:ml-auto">
