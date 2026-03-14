958e093060287b2d3e2b50bfc70a3b31ce761298
SlickYeet <lasse@lx2.dev>
2026-03-14 14:49:24 -0700

    feat: add inferAdditionalFields plugin to authClient

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -1,15 +1,22 @@
 import {
   adminClient,
+  inferAdditionalFields,
   lastLoginMethodClient,
   twoFactorClient,
 } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 
 import { env } from "@/env"
+import type { auth } from "@/server/auth"
 
 export const authClient = createAuthClient({
   baseURL: env.NEXT_PUBLIC_URL,
-  plugins: [adminClient(), lastLoginMethodClient(), twoFactorClient()],
+  plugins: [
+    adminClient(),
+    lastLoginMethodClient(),
+    twoFactorClient(),
+    inferAdditionalFields<typeof auth>(),
+  ],
 })
 
 export const { signIn, signOut, useSession } = authClient
