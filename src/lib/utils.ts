262091879596edb16fdffd239efccf9c4db670cc
SlickYeet <lasse@lx2.dev>
2026-03-15 00:04:46 -0700

    feat: add @node-rs/argon2 dependency and implement password hashing utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,22 @@
+import type { Options } from "@node-rs/argon2"
+import { hash } from "@node-rs/argon2"
 import type { ClassValue } from "clsx"
 import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+const opts: Options = {
+  algorithm: 2,
+  memoryCost: 65536,
+  outputLen: 32,
+  parallelism: 4,
+  timeCost: 3,
+}
+
+export async function hashPassword(password: string) {
+  const res = await hash(password, opts)
+  return res
 }
